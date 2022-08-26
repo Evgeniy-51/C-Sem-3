76a5341c0ac134b9471a3406d1f5c6669d35f6c7
Evgeniy-51 <evgeniy-51@yandex.ru>
2022-08-27 00:33:56 +0300

    init

---
+++ task1/task1.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите пятизначное число:  ");
+int num = Int32.Parse(Console.ReadLine());
+
+int dig1 = num / 10000;
+int dig2 = (num / 1000) % 10;
+int dig4 = (num / 10) % 10;
+int dig5 = num % 10;
+
+Console.Write((dig1 == dig5 && dig2 == dig4) ? "Да" : "Нет");
+
+
