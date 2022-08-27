e86f055aacb9fee5972006b87c7e4d81250bfb37
Evgeniy-51 <evgeniy-51@yandex.ru>
2022-08-27 11:47:37 +0300

    task3

---
+++ task3/task3.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Enter a number:  ");
+int n = Int32.Parse(Console.ReadLine());
+
+int[] arr = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    arr[i] = (i + 1) * (i + 1) * (i + 1);
+    // arr[i] = Int32.Parse(Math.Pow(3, 3));
+}
+
+Console.WriteLine(String.Join(", ", arr));
